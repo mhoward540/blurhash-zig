b8254859599ce80583b00b87c3fb3470142aad36
Matt Howard <mjhoward540@gmail.com>
2025-01-07 17:35:55 +0100

    Update for compat with zig 0.13.0 (#4)
    * Update build
    * re-pin dependencies
    * update readme
    * update .gitignore to reflect new cache directory
    * Bump version in zon file

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "blurhash-zig",
-        .root_source_file = .{ .path = "main.zig" },
+        .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "main.zig" },
+        .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = optimize,
     });
